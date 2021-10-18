12adca0e8e184525f21b387f9be62a0035f843ef
joery12 <90188965+joery12@users.noreply.github.com>
2021-10-18 20:38:11 +0200

    Add files via upload

---
+++ function_arguments.js
@@ -0,0 +1,7 @@
+const wallPaint = function(wall, color) {
+    console.log("The " + wall + "wall has been painted " + color + ".");
+
+};
+
+wallPaint("north", "green");
+wallPaint("south", "blue");
